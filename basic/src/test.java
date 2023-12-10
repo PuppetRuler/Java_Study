4b20d2dc6590c6cd3e367223bfa5a8667e8496bd
PuppetRuler <ziyelixiang@gmail.com>
2023-12-10 23:55:05 +0800

    first commit
    first commit

---
+++ basic/src/test.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+/**
+ * ClassName:test
+ * Description:
+ *
+ * @Author Sixty-three
+ * @Create 2023/12/9 23:34
+ * @Version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        int[] i = new int[]{1,2,34,4};
+        System.out.println(Arrays.toString(i));
+    }
+}
